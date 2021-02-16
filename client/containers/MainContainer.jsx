5fcd31f1e6aceeb2feb1b81e1aaf95949122b279
tony <anthony@amartinez.cc>
2021-02-16 15:52:08 -0800

    init project

---
+++ client/containers/MainContainer.jsx
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container'
+import { Col, Row } from 'react-bootstrap';
+
+import PetContainer from './PetContainer';
+import Navigation from '../components/Navigation';
+import styles from '../styles.css';
+import realFakeData from '../FakeModel/realFakeData';
+import fakeData from '../FakeModel/fakeData';
+
+class MainContainer extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.state.fetchedPetData = [];
+    this.state.zipcode = '';
+    this.state.type = '';
+    this.state.favs = [];
+    this.state.indexPage = true;
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.handleTypeChange = this.handleTypeChange.bind(this);
+  }
+
+  handleTypeChange(event) {
+    console.log('inside handleTypeChange')
+    console.log(event.target.value)
+
+    this.setState({value: event.target.value});
+
+  }
+
+  //handle search requests
+  handleSearch(e){
+    event.preventDefault();
+    console.log(event.target.value)
+    console.log('inside handleSearch')
+    let zipcode = this.state.zipcode;
+    let type = this.state.type;
+    if (event.target.value === 'Cat' || event.target.value === 'Dog'){
+      console.log('inside if type change', event.target.value)
+      this.setState({
+        ...this.state, 
+        type: event.target.value
+      })
+    }
+    else if(Number(event.target.value)){
+      console.log('inside if zipcode change', event.target.value)
+      console.log('inside zipcode')
+      this.setState({
+        ...this.state, 
+        zipcode: event.target.value
+      })
+    }
+
+    else if (e == 'submit'){
+      console.log(this.state.type)
+      let fetchURL;
+      if (this.state.type === ''){
+        fetchURL = 'petfinder/dog'  + '/' + this.state.zipcode;
+      }
+      else {
+        fetchURL = 'petfinder/' + this.state.type + '/' + this.state.zipcode;
+      }
+      console.log(fetchURL)
+      fetch(fetchURL)
+        .then(data => data.json())
+        .then(data => {
+        console.log(data)
+        this.setState({
+          ...this.state,
+          fetchedPetData: data,
+          indexPage: false,
+        })
+    })
+    }
+    //send fetch request 
+    
+    //parse through retrieved data
+    //update state to render 
+  }
+
+  //handle pet saving events
+  handleSave(saveId){
+    event.preventDefault();
+    console.log(event.target)
+    //send fetch (Post) request here
+    const data = {
+      id: saveId,  
+    }
+    fetch('/db', {
+    method: 'POST',
+    headers: {
+      'Content-Type' : 'application/json',
+    },
+    body: JSON.stringify(data)
+    })
+    // .then(fetch('/getmessages/')
+    .then(data => data.json())
+    .then(data => {
+    //parse through retrieved data
+    //update state to render 
+    this.setState({
+      ...this.state,
+    })
+  })
+  }
+
+  render() {
+    console.log(this.state)
+    //render index page version of MainContainer at initial render
+    if (this.state.indexPage){
+      return (
+        <Container>
+          <Row>
+            <Col> 
+              <Navigation 
+                indexPage = {this.state.indexPage}
+                handleSearch = {this.handleSearch} /> 
+            </Col>
+          </Row>
+        </Container>
+      )
+    }
+
+    //render MainContainer with Navigation at all other times
+    if (!this.state.indexPage){
+      return (
+        <div>
+          <Navigation 
+          handleSearch = {this.handleSearch}
+          handleTypeChange = {this.handleTypeChange}
+
+          />
+          <PetContainer
+          handleSave = {this.handleSave} fetchedPetData = {this.state.fetchedPetData}/>
+        </div>
+      )
+    }
+  }   
+}
+
+export default MainContainer;
---
+++ client/containers/PetContainer.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Container from 'react-bootstrap/Container';
+import { Col, Row } from 'react-bootstrap';
+import PetDetails from '../components/PetDetails'
+// import PetImages from '..components/PetImages';
+// import SavePet from '..components/SavePet';
+// import data from '../FakeModel/fakeData';
+
+
+function PetContainer(props){  
+  console.log('pet container props: ', props)
+
+  const dataObjects = props.fetchedPetData.animals.map((obj, i)=>{
+    return(<PetDetails
+      key = {i}
+      id = {obj.id}
+      name = {obj.name}
+      age = {obj.age}
+      gender = {obj.gender}
+      description = {obj.description}
+      primaryPhoto = {obj.primary_photo_cropped}
+      photos = {obj.photos}
+    />)
+  })
+
+  
+  return(
+    <Container>
+      <Row className="justify-content-md-center">
+      <Col></Col>
+        <Col>
+          <h3>Pets Near You</h3>
+          {dataObjects}
+          </Col>
+        <Col></Col>
+      
+      </Row>
+    </Container>
+  );
+}
+  
+
+
+// Render an <App> component to the #app div in the body
+export default PetContainer;
